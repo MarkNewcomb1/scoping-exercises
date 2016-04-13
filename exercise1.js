6cfd214054b9a486b083fb6e3a4f51b62a0536da
MarkNewcomb <marknewcomb@Marks-MacBook-Pro.local>
2016-04-13 13:22:52 -0600

    initial commit

---
+++ exercise1.js
@@ -0,0 +1,23 @@
+/*
+	Exercise 1
+	The following code should pring "Homer", "Barney", "Marge"
+	How should we fix this?
+*/
+function createCharacter(width, height, color, name) {
+	widget = {
+		width: width,
+		height: height,
+		color: color,
+		name: name
+	};
+	return widget;
+}
+
+widget = createCharacter(10, 12, "yellow", "Homer");
+widget2 = createCharacter(12, 18, "blue", "Barney");
+widget3 = createCharacter(4, 7, "red", "Marge");
+
+//Hey! Don't re-order these.
+console.log(widget.name);
+console.log(widget2.name);
+console.log(widget3.name);
